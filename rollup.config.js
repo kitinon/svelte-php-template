9c3572b2ea55708d279f53f1c8faa131837ecd6d
Kitinon Wangpattanamongkol <kitinon@gmail.com>
2019-05-16 07:44:50 +0700

    No css bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -20,9 +20,9 @@ export default {
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			},
+			// css: css => {
+			//	css.write('public/bundle.css');
+			// },
 			customElement: true
 		}),
 
