25bd4dc2dd645aea114c4cb13b66a31b15526028
tanangular <tanangular@gmail.com>
2019-10-05 16:32:32 +0700

    custom theme form css bootstrap example

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,7 @@
 <svelte:options tag="s-app"/>
-
-<script>
-	export let name;
-</script>
-
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<svelte:head>
+	<link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+	<link rel='icon' type='image/png' href='favicon.png'>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel='stylesheet' href='global.css'>
+</svelte:head>
